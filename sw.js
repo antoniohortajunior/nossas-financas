839bca3949e7eff481ce084dc2a5144d990ad7fe
antoniohortajunior <140515165+antoniohortajunior@users.noreply.github.com>
2026-09-06 16:32:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "minhas-financas-v5";
+const CACHE = "minhas-financas-v6";
 const CORE = ["./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icons/icon.png", "./icons/icon192.png", "./icons/icon512.png"];
 
 self.addEventListener("install", (event) => {
